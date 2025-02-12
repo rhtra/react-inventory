6c4bf709a13f8289add979a2b58735dc15c1259b
Arthur Buenaventura <arthur@Arthurs-MacBook-Pro.local>
2025-02-12 14:15:04 +0800

    Change name

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -68,7 +68,7 @@ export default function NavBarComponent() {
                     textDecoration: "none",
                   }}
                 >
-                  ADIMS
+                  React Inventory
                 </Typography>
 
                 <Box
